5b68a0a4f0a72fd9c5a5b6a1cdaf0b153b434f52
Sajid Ibne Anower <sajid.anower@skutopia.com>
2023-12-25 16:42:46 +1100

    Pass in params and handle error

--- src/features/emoji_contributor.rs
+++ src/features/emoji_contributor.rs
@@ -1,18 +1,34 @@
-use rocket::get;
-use rocket::Route;
+use rocket;
+use rocket::{get, Route};
 use std::env;
 use crate::slack::client::SlackClient;
-use rocket;
+use crate::slack::emoji::EmojiListParams;
 
 #[get("/emoji-contributor")]
 pub async fn emoji_contributor_route() -> &'static str {
     let token = env::var("SLACK_TOKEN").expect("Please set SLACK_TOKEN");
     let slack_client = SlackClient::new(&token);
-    let emoji_list_response = slack_client.emoji().list().await.unwrap();
-    let emoji_list = emoji_list_response.emoji;
-    let emoji_list_keys = emoji_list.keys();
-    print!("{:?}", emoji_list_keys);
-    return "Emoji Contributor - not implemented yet"
+    let params = Some(EmojiListParams {
+        pretty: 1,
+        include_categories: false,
+    });
+    let emoji_list_response = slack_client.emoji().list(params).await;
+    match emoji_list_response {
+        Ok(emoji_list_response) => {
+            let emoji_list = emoji_list_response.emoji;
+            let emoji_list_keys = emoji_list.keys();
+            println!("{:?}", emoji_list_keys);
+            if !emoji_list_response.ok {
+                return "Emoji Contributor - encountered error";
+            } else {
+                return "Emoji Contributor - not implemented yet";
+            }
+        },
+        Err(error) => {
+            println!("Encountered error: {:?}", error);
+            return "Could not get emoji list";
+        },
+    }
 }
 
 pub fn routes() -> Vec<Route> {
